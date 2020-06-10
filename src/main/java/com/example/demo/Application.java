865d17d6d3d7b29aa5a790943126e98e08ae47d8
Phea Soy <pheasoy@eBiz-admins-MacBook-Pro.local>
2020-06-10 21:30:04 +0700

    Adding liquibase programmatically

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,18 +1,32 @@
 package com.example.demo;
 
+import liquibase.integration.spring.SpringLiquibase;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+@Slf4j
+public class Application
+        implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		app().run(args);
+    @Autowired
+    private SpringLiquibase liquibase;
 
-	}
+    public static void main(String[] args) {
+        app().run(args);
 
-	public static SpringApplication app(){
-		return new SpringApplication(Application.class);
-	}
+    }
 
+    public static SpringApplication app() {
+        return new SpringApplication(Application.class);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Liquibase changelog:{}", liquibase.getChangeLog());
+    }
 }
